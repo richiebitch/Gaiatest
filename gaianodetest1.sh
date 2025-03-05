5059adc119a176f09c40c1393fd7bb97a4d01658
abhiag <36300569+abhiag@users.noreply.github.com>
2025-03-06 04:00:30 +0530

    Update gaianodetest1.sh

--- gaianodetest1.sh
+++ gaianodetest1.sh
@@ -364,8 +364,8 @@ case $choice in
             else
                 # Check for NVIDIA GPU and install CUDA if available
                 if check_nvidia_gpu; then
-                    if ! check_cuda_installed; then
-                        setup_cuda_env
+                    if ! setup_cuda_env; then
+                        check_cuda_installed
                         install_cuda
                     else
                         echo "⚠️ CUDA is already installed. Skipping CUDA installation."
