79ca888893ce7e0fe137e84e0b67cc8b9c73b04a
abhiag <36300569+abhiag@users.noreply.github.com>
2025-03-07 21:10:50 +0530

    Update 2.sh

--- 2.sh
+++ 2.sh
@@ -170,22 +170,22 @@ set_config_url() {
     echo "🔧 Detected System Type: $SYSTEM_TYPE"
 
     if [[ $SYSTEM_TYPE -eq 0 ]]; then
-        CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config2.json"
+        CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config.json"
     elif [[ $SYSTEM_TYPE -eq 1 ]]; then
         if ! check_nvidia_gpu; then
-            CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config2.json"
+            CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config.json"
         else
-            CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config1.json"
+            CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config.json"
         fi
     elif [[ $SYSTEM_TYPE -eq 2 ]]; then
         if ! check_nvidia_gpu; then
-            CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config2.json"
+            CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config.json"
         else
-            CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config3.json"
+            CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config.json"
         fi
     else
         echo "⚠️ Unable to determine system type. Using default configuration."
-        CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config2.json"
+        CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/config.json"
     fi
     echo "🔗 Using configuration: $CONFIG_URL"
 }
