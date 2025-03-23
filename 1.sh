6c5987ecd24365245ae03d07a2579d91e65ba408
abhiag <36300569+abhiag@users.noreply.github.com>
2025-03-23 13:10:49 +0530

    Update 1.sh

--- 1.sh
+++ 1.sh
@@ -206,7 +206,7 @@ elif [[ $SYSTEM_TYPE -eq 1 ]]; then
     if ! check_nvidia_gpu; then
         CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/Hyper_3.2-3B.json"
     else
-        CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/Soneium2.5-0.5B.json"
+        CONFIG_URL="https://raw.githubusercontent.com/abhiag/Gaia_Node/main/Deepseek8.json"
     fi
 elif [[ $SYSTEM_TYPE -eq 2 ]]; then
     # Desktop
