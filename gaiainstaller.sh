74e861117694c4ac3607e429fdb43bc46f2667de
richiebitch <ankit.gehlot1947@gmail.com>
2025-04-03 23:53:08 +0530

    gaiainstaller.sh

--- gaiainstaller.sh
+++ gaiainstaller.sh
@@ -194,8 +194,7 @@ echo "==============================================================="
 
                 sleep 5
                 screen -r gaiabot
-            fi
-            ;;
+          
 
         5)
             # Function to list active screen sessions and allow user to select one
